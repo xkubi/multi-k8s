821dc73af90ea4e756e39ec19b5320397aa3de6e
xkubi <kubicajanko@gmail.com>
2023-09-24 17:58:16 +0200

    adding change

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React ad
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
